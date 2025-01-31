f3d30982fdcd944f1c62caad43fc29cd01590ee8
canersekerci <canerskrc29@gmail.com>
2025-01-31 22:00:08 +0300

    commit update

---
+++ Top_fb/good_nodes.py
@@ -0,0 +1,28 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def goodNodes(self, root: 'TreeNode') -> 'TreeNode':
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+            return res
+
+        return dfs(root, root.val)
+root = TreeNode(9)
+root.right = TreeNode(3)
+root.left.left = TreeNode(3)
+root.right.left = TreeNode(1)
+root.right.right = TreeNode(5)
+
+solution = Solution()
+
+print(solution.goodNodes(root))
