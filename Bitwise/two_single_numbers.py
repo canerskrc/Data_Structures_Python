5eb478c102a0357a03325a42dda991ba84406cff
canersekerci <canerskrc29@gmail.com>
2025-04-21 00:08:12 +0300

    uploaded Bitwise

---
+++ Bitwise/two_single_numbers.py
@@ -0,0 +1,9 @@
+def find_single_numbers(nums):
+    # TODO: Write your code here
+    return [-1, -1]
+
+
+def main():
+    print('Single numbers are:' + str(find_single_numbers([2, 1, 3, 2])))
+
+main()
